55e36375e94b728b8036cc9d22a9c0cf9d90b977
Rehab AlNairi <133367687+Rehabalnairi@users.noreply.github.com>
2025-05-29 16:14:47 +0400

    adding Reflection Questions

--- backup-EMP2.sql
+++ backup-EMP2.sql
@@ -57,3 +57,25 @@ RESTORE DATABASE TrainingDB WITH RECOVERY;
 --Step 3: Verify the Restored Data
 USE TrainingDB; 
 SELECT * FROM Students; 
+
+1. What would happen if you skipped the differential backup step? 
+ the atabase will only reflect the data state at the time of the full backup.
+
+2. What’s the difference between restoring a full vs. copy-only backup? 
+thhe only difference between full and full-copy is that full-copy does not break the differential chain. Neither of them breaks the log chain as 
+neither of them truncates the log file.
+
+3. What happens if you use WITH RECOVERY in the middle of a restore chain? 
+sing WITH RECOVERY too early ends the restore process and brings the database online.
+
+ You can no longer apply additional backups (like differential or log backups) after this.
+
+ You should only use WITH RECOVERY at the final step of the restore chain.
+
+ Use WITH NORECOVERY until all parts are restored, then finish with WITH RECOVERY
+
+4. Which backup types are optional and which are mandatory for full recovery?
+For full data recovery, full backups are mandatory. Differential and 
+incremental backups are often used in conjunction with full backups to optimize storage 
+and recovery time, but they are 
+not sufficient on their own for a full restoration are not sufficient on their own for a full restoration
